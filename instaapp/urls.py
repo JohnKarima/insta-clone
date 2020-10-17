286fbf5b122b3303f9790a3c6c4a03cf82f96c85
John Karima <karimajohn24@gmail.com>
2020-10-17 12:42:06 +0300

    create login, logout.html pages

--- instaapp/urls.py
+++ instaapp/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns=[
     url(r'^$',views.index,name = 'index'),
     url(r'^register/',views.register,name = 'register'),
+    url(r'^login/',auth_views.LoginView.as_view(template_name='users/login.html'),name = 'login'),
+    url(r'^login/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name = 'logout'),
 ]
