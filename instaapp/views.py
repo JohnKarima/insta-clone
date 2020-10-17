c121366b3d62c3a8a9eb4a12499a986ba247bae8
John Karima <karimajohn24@gmail.com>
2020-10-17 23:12:15 +0300

    split profile and updateProfile functionality and pages

--- instaapp/views.py
+++ instaapp/views.py
@@ -45,6 +45,35 @@ def register(request):
 
 @login_required
 def profile(request):
+    images = Image.objects.all()
+
+    # if request.method == "POST":   
+    #     u_form = UserUpdateForm(request.POST, instance=request.user)
+    #     p_form = ProfileUpdateForm(request.POST, request.FILES, 
+    #     instance=request.user.profile)
+
+    #     if u_form.is_valid() and p_form.is_valid():
+    #         u_form.save()
+    #         p_form.save()
+    #         messages.success(request, f'Successfully updated your account!')
+    #         return redirect('profile')
+
+    
+    # else:   
+    #     u_form = UserUpdateForm(instance=request.user)
+    #     p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    # context = {
+    #     'u_form': u_form,
+    #     'p_form': p_form
+    # }   
+
+    return render(request, 'users/profile.html', {"images":images[::-1]})
+
+
+
+@login_required
+def update(request):
     if request.method == "POST":   
         u_form = UserUpdateForm(request.POST, instance=request.user)
         p_form = ProfileUpdateForm(request.POST, request.FILES, 
@@ -66,6 +95,10 @@ def profile(request):
         'p_form': p_form
     }
 
-    return render(request, 'users/profile.html', context)
+    return render(request, 'users/update.html', context)
+
+
+
+
 
 
