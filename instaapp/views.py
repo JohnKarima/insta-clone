3aa4453cc89510f4bea76fc8c4ddc9bccced8a69
John Karima <karimajohn24@gmail.com>
2020-10-17 09:44:32 +0300

    make initial migrations

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Welcome to Instagram')
+    return render(request, 'index.html')
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Successfully created account created for {username}!')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/register.html', {'form':form})
 
