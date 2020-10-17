ac99e64f8d3f9e66bc1b403622242f556cd84b78
John Karima <karimajohn24@gmail.com>
2020-10-17 09:23:55 +0300

    create register.html and add register form

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse('Welcome to Instagram')
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form':form})
+
