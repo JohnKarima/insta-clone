f01fce909742eaf2a7ce04da269afad776d41342
John Karima <karimajohn24@gmail.com>
2020-10-17 10:01:24 +0300

    crete forms.py, add UserRegistrationForm class

--- instaapp/views.py
+++ instaapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def index(request):
@@ -9,12 +9,13 @@ def index(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Successfully created account created for {username}!')
             return redirect('index')
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form':form})
 
