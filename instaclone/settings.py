769800e30fd3a73d97d9da5583bbc515e6353b6c
John Karima <karimajohn24@gmail.com>
2020-10-19 02:32:32 +0300

    style registration form

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'instaapp.apps.InstaappConfig',
-    'bootstrap3',
+    # 'bootstrap3',
     'crispy_forms',
     'cloudinary',
 ]
@@ -129,13 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
